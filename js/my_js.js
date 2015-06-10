c7322e612d02bbff4e4c1a3010b802cca7c510d3
kienlu <lonelyleo30071994@gmail.com>
2015-06-10 15:21:01 +0700

    KhachHang
    GiaoDienDauTien

---
+++ js/my_js.js
@@ -0,0 +1,16 @@
+function check_empty() {
+if (document.getElementById('name').value == "" || document.getElementById('pass').value == "" ) {
+alert("Fill All Fields !");
+} else {
+document.getElementById('form').submit();
+alert("Form Submitted Successfully...");
+}
+}
+//Function To Display Popup
+function div_show() {
+document.getElementById('logindiv').style.display = "block";
+}
+//Function to Hide Popup
+function div_hide(){
+document.getElementById('logindiv').style.display = "none";
+}
